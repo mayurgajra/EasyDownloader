19a4787de24235bd95bef0315d322edd545f21bc
Mayur Gajra <mayurgajra5@gmail.com>
2022-02-21 07:12:32 +0530

    - Updated gradle to kts & created buildSrc module for manging dependency

--- app/src/main/java/com/mayurg/easydownloader/ui/theme/Color.kt
+++ app/src/main/java/com/mayurg/easydownloader/ui/theme/Color.kt
@@ -3,6 +3,6 @@ package com.mayurg.easydownloader.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
+val Purple500 = Color(0xFFDA2644)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
