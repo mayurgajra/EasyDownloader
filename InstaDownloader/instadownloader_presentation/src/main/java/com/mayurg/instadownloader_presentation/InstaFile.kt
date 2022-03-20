0eedec3722d307b175715a0e7bd48c00dab66467
Mayur Gajra <mayurgajra5@gmail.com>
2022-03-20 18:22:37 +0530

    - Insta file list setup started

---
+++ InstaDownloader/instadownloader_presentation/src/main/java/com/mayurg/instadownloader_presentation/InstaFile.kt
@@ -0,0 +1,5 @@
+package com.mayurg.instadownloader_presentation
+
+data class InstaFile(
+    val name: String = ""
+)
--- InstaDownloader/instadownloader_presentation/src/main/java/com/mayurg/instadownloader_presentation/InstagramTab.kt
+++ InstaDownloader/instadownloader_presentation/src/main/java/com/mayurg/instadownloader_presentation/InstagramTab.kt
@@ -1,11 +1,11 @@
 package com.mayurg.instadownloader_presentation
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -13,9 +13,16 @@ fun InstagramTab() {
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Insta")
+        FilesList(messages = listOf(InstaFile("cdc")))
+    }
+}
+
+@Composable
+fun FilesList(messages: List<InstaFile>) {
+    LazyColumn {
+        items(messages) { message ->
+            Text(text = "Insta File ${message.name}")
+        }
     }
 }
