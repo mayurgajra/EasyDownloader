5460a821b7932c023de10ce63307e137c2ba6480
Mayur Gajra <mayurgajra5@gmail.com>
2022-04-23 10:51:06 +0530

    - Sorted files by modified time descending

--- FileManager/src/main/java/com/mayurg/filemanager/FileManager.kt
+++ FileManager/src/main/java/com/mayurg/filemanager/FileManager.kt
@@ -21,19 +21,25 @@ class FileManager private constructor(
 
     fun loadInstaFiles(path: String): MutableList<File> {
 
-        val file: File
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            file =
-                File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)?.absolutePath + path)
+        val file: File = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)?.absolutePath + path)
         } else {
-            file = File(Environment.getExternalStorageDirectory().absolutePath + path)
+            File(Environment.getExternalStorageDirectory().absolutePath + path)
         }
 
         if (!file.exists()) {
             file.mkdirs()
         }
 
-        return file.listFiles()?.toMutableList() ?: mutableListOf()
+        val files = mutableListOf<File>()
+
+        file.listFiles()?.toMutableList()?.let { list ->
+            list.sortByDescending { it.lastModified() }
+            files.addAll(list)
+        }
+
+
+        return files
     }
 
 
